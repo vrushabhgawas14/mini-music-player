cd9217e4f5eaa659a3538f446d760c0e5d32fa76
Vrushabh Gawas <vrushabhgawas14@gmail.com>
2024-01-31 23:16:01 +0530

    added pics,audio and js

--- assests/script.js
+++ assests/script.js
@@ -5,10 +5,54 @@ function changeImg(){
     let artistImg = document.querySelector(".song-img");
     if (audio.paused) {
         audio.play();
-        img.src = "./assests/imgs/pause.svg";
+        img.src = "./assests/imgs/svgs/pause.svg";
         artistImg.classList.add("rotate");
     }else{
         audio.pause();
-        img.src = "./assests/imgs/play.svg";
+        img.src = "./assests/imgs/svgs/play.svg";
     }
 }
+
+let audio = document.getElementById('player');
+let artistImg = document.querySelector(".artistImg");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+let prev_track = document.querySelector(".prev-track");
+let next_track = document.querySelector(".next-track");
+let play_pause_track =document.querySelector(".curr-track");
+let curr_track = document.createElement("audio");
+
+let track_index = 0;
+let isPlaying = true;
+let updateTimer;
+
+const musicList = [
+    {
+        img: './assests/imgs/stephenSong.jpg',
+        name: 'Until I found her',
+        artist: 'Stephen Sanchez',
+        music: './assests/audio/Until_i_FoundYou.mpeg'
+    },
+    {
+        img: './assests/imgs/harry.jpg',
+        name: 'As It Was',
+        artist: 'Harry Styles',
+        music: './assests/audio/AsItWas.mp3'
+    },
+    {
+        img: './assests/imgs/Fran.jpg',
+        name: 'Hypothetcials',
+        artist: 'Fran Vasilić',
+        music: './assests/audio/Hypotheticals.mp3'
+    },{
+        img: './assests/imgs/makeUmine.jpg',
+        name: 'Make You Mine',
+        artist: 'PUBLIC',
+        music: './assests/audio/MakeYouMine.mp3'
+    },{
+        img: './assests/imgs/jon.jpg',
+        name: 'Roses are Red',
+        artist: 'Jon Caryl',
+        music: './assests/audio/RosesAreRed.m4a'
+    }
+]
