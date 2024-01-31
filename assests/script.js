3d1726bad534b5e3329f5e52476b3ceffffe4cbb
Vrushabh Gawas <vrushabhgawas14@gmail.com>
2024-01-31 21:20:22 +0530

    Image Rotation

--- assests/script.js
+++ assests/script.js
@@ -2,9 +2,11 @@
 function changeImg(){
     let img = document.getElementById("secondImg");
     let audio = document.getElementById('player');
+    let artistImg = document.querySelector(".song-img");
     if (audio.paused) {
         audio.play();
         img.src = "./assests/imgs/pause.svg";
+        artistImg.classList.add("rotate");
     }else{
         audio.pause();
         img.src = "./assests/imgs/play.svg";
