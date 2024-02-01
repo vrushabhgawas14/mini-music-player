81114f8d58a25ae7d1482139f4ac59369cb4aebb
Vrushabh Gawas <vrushabhgawas14@gmail.com>
2024-02-01 17:54:02 +0530

    JS for prev next and curr play

--- assests/script.js
+++ assests/script.js
@@ -1,44 +1,25 @@
 
-function changeImg(){
-    let img = document.getElementById("secondImg");
-    let audio = document.getElementById('player');
-    let artistImg = document.querySelector(".song-img");
-    if (audio.paused) {
-        audio.load();
-        audio.play();
-        img.src = "./assests/imgs/svgs/pause.svg";
-        artistImg.classList.add("rotate");
-    }else{
-        audio.pause();
-        img.src = "./assests/imgs/svgs/play.svg";
-    }
-}
-
-let audio = document.getElementById('player');
-let artistImg = document.querySelector(".artistImg");
+let playpauseImg = document.querySelector("#secondImg");
+let artistImg = document.querySelector(".song-img");
 let track_name = document.querySelector(".track-name");
 let track_artist = document.querySelector(".track-artist");
-let prev_track = document.querySelector(".prev-track");
-let next_track = document.querySelector(".next-track");
-let play_pause_track =document.querySelector(".curr-track");
-let curr_track = document.createElement("audio");
+let curr_track = document.getElementById('player');
 
 let track_index = 0;
-let isPlaying = true;
-let updateTimer;
+let isPlaying = false;
 
 const musicList = [
-    {
-        img: './assests/imgs/stephenSong.jpg',
-        name: 'Until I found her',
-        artist: 'Stephen Sanchez',
-        music: './assests/audio/Until_i_FoundYou.mpeg'
-    },
     {
         img: './assests/imgs/harry.jpg',
         name: 'As It Was',
         artist: 'Harry Styles',
         music: './assests/audio/AsItWas.mp3'
+    },
+    {
+        img: './assests/imgs/stephenSong.jpg',
+        name: 'Until I found her',
+        artist: 'Stephen Sanchez',
+        music: './assests/audio/Until_i_FoundYou.mpeg'
     },
     {
         img: './assests/imgs/Fran.jpg',
@@ -57,3 +38,53 @@ const musicList = [
         music: './assests/audio/RosesAreRed.m4a'
     }
 ]
+
+loadTrack(track_index);
+function loadTrack(track_index){
+    curr_track.src = musicList[track_index].music;
+    curr_track.load();
+
+    artistImg.src = musicList[track_index].img;
+    track_name.textContent = musicList[track_index].name;
+    track_artist.textContent = musicList[track_index].artist;
+
+    curr_track.addEventListener('ended',nextTrack);
+}
+
+function nextTrack(){
+    if(track_index < musicList.length -1){
+        track_index += 1;
+    }else{
+        track_index = 0;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+
+function prevTrack(){
+    if(track_index > 0){
+        track_index -= 1;
+    }else{
+        track_index = musicList.length -1;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+
+
+function playPauseTrack(){
+    isPlaying ? pauseTrack() : playTrack();
+}
+
+function playTrack(){
+    curr_track.play();
+    isPlaying = true;
+    artistImg.classList.add("rotate");
+    playpauseImg.src = "./assests/imgs/svgs/pause.svg";
+}
+
+function pauseTrack(){
+    curr_track.pause();
+    isPlaying = false;
+    playpauseImg.src = "./assests/imgs/svgs/play.svg";
+}
