56e6d30c7c023bf4ed802f9424b260748e48fd77
Vrushabh Gawas <vrushabhgawas14@gmail.com>
2024-02-02 18:18:41 +0530

    Added More languages,Hindi and Marathi

--- assests/script.js
+++ assests/script.js
@@ -8,51 +8,123 @@ let curr_track = document.getElementById('player');
 let track_index = 0;
 let isPlaying = false;
 
-const musicList = [
+let englang = document.querySelector(".eng");
+let marlang = document.querySelector(".mar");
+let hinlang = document.querySelector(".hin");
+let dropdown = document.querySelector(".drop-down");
+
+function selectLang(){
+    if (dropdown.style.visibility == 'visible') {
+        dropdown.style.visibility = 'hidden';
+    }else{
+        dropdown.style.visibility = 'visible';
+    }
+}
+
+englang.addEventListener('click',engFunc);
+marlang.addEventListener('click',marFunc);
+hinlang.addEventListener('click',hinFunc);
+
+function engFunc(){
+    localStorage.setItem("myData","English");
+    document.location.reload();
+}
+
+function marFunc(){
+    localStorage.setItem("myData","Marathi");
+    document.location.reload();
+}
+
+function hinFunc(){
+    localStorage.setItem("myData","Hindi");
+    document.location.reload();
+}
+
+let currMusicList = [];
+
+const englishList = [
     {
-        img: './assests/imgs/harry.jpg',
-        name: 'As It Was',
-        artist: 'Harry Styles',
-        music: './assests/audio/AsItWas.mp3'
+        "img": "./assests/imgs/English/harry.jpg",
+        "name": "As It Was",
+        "artist": "Harry Styles",
+        "music": "./assests/audio/English/AsItWas.mp3"
     },
     {
-        img: './assests/imgs/stephenSong.jpg',
-        name: 'Until I found her',
-        artist: 'Stephen Sanchez',
-        music: './assests/audio/Until_i_FoundYou.mpeg'
+        "img": "./assests/imgs/English/stephenSong.jpg",
+        "name": "Until I found her",
+        "artist": "Stephen Sanchez",
+        "music": "./assests/audio/English/Until_i_FoundYou.mpeg"
     },
     {
-        img: './assests/imgs/Fran.jpg',
-        name: 'Hypothetcials',
-        artist: 'Fran Vasilić',
-        music: './assests/audio/Hypotheticals.mp3'
+        "img": "./assests/imgs/English/Fran1.jpg",
+        "name": "Hypothetcials",
+        "artist": "Fran Vasilić",
+        "music": "./assests/audio/English/Hypotheticals.mp3"
     },{
-        img: './assests/imgs/makeUmine.jpg',
-        name: 'Make You Mine',
-        artist: 'PUBLIC',
-        music: './assests/audio/MakeYouMine.mp3'
+        "img": "./assests/imgs/English/makeUmine1.jpg",
+        "name": "Make You Mine",
+        "artist": "PUBLIC",
+        "music": "./assests/audio/English/MakeYouMine.mp3"
     },{
-        img: './assests/imgs/jon.jpg',
-        name: 'Roses are Red',
-        artist: 'Jon Caryl',
-        music: './assests/audio/RosesAreRed.m4a'
+        "img": "./assests/imgs/English/jon.jpg",
+        "name": "Roses are Red",
+        "artist": "Jon Caryl",
+        "music": "./assests/audio/English/RosesAreRed.m4a"
     }
 ]
 
+const marathiList = [
+    {
+        img: './assests/imgs/Marathi/vrushabh-crop.jpg',
+        name: 'Darling',
+        artist: 'Vrushabh Gawas',
+        music: './assests/audio/Marathi/InTheEnd.mp3'
+    }
+]
+
+const hindiList = [
+    {
+        img: './assests/imgs/Hindi/mujheMK.jpg',
+        name: 'Mujh Mein Kahin',
+        artist: 'Ajay Atul',
+        music: './assests/audio/Hindi/MujhMeinKahin.mp3'
+    },
+    {
+        img: './assests/imgs/Hindi/DeMZ.jpg',
+        name: 'De Mauka Zindagi',
+        artist: 'Nilotpal Bora',
+        music: './assests/audio/Hindi/DeMaukaZindagi.mp3'
+    },
+    {
+        img: './assests/imgs/Hindi/Dangal.jpg',
+        name: 'Gilehriyaan',
+        artist: 'Pritam',
+        music: './assests/audio/Hindi/Gilehriyaan.mp3'
+    }
+]
+
+if (localStorage.getItem("myData") == "Marathi") {
+    currMusicList = marathiList;
+}else if(localStorage.getItem("myData") == "Hindi"){
+    currMusicList = hindiList;
+}else{
+    currMusicList = englishList;
+}
+
 loadTrack(track_index);
 function loadTrack(track_index){
-    curr_track.src = musicList[track_index].music;
+    curr_track.src = currMusicList[track_index].music;
     curr_track.load();
 
-    artistImg.src = musicList[track_index].img;
-    track_name.textContent = musicList[track_index].name;
-    track_artist.textContent = musicList[track_index].artist;
+    artistImg.src = currMusicList[track_index].img;
+    track_name.textContent = currMusicList[track_index].name;
+    track_artist.textContent = currMusicList[track_index].artist;
 
     curr_track.addEventListener('ended',nextTrack);
 }
 
 function nextTrack(){
-    if(track_index < musicList.length -1){
+    if(track_index < currMusicList.length -1){
         track_index += 1;
     }else{
         track_index = 0;
@@ -65,7 +137,7 @@ function prevTrack(){
     if(track_index > 0){
         track_index -= 1;
     }else{
-        track_index = musicList.length -1;
+        track_index = currMusicList.length -1;
     }
     loadTrack(track_index);
     playTrack();
@@ -88,4 +160,3 @@ function pauseTrack(){
     isPlaying = false;
     playpauseImg.src = "./assests/imgs/svgs/play.svg";
 }
-
